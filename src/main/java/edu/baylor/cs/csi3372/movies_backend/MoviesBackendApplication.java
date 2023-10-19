55110b430a1a3d8c6a141901d8f1bd27d02bfe5d
Asif Imtiaz Shaafi <shaafi.20994@gmail.com>
2023-10-18 23:31:26 -0500

    Initialized project

---
+++ src/main/java/edu/baylor/cs/csi3372/movies_backend/MoviesBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.baylor.cs.csi3372.movies_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesBackendApplication.class, args);
+	}
+
+}
